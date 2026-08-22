da7a0be29196b9d2aff649ea7e26972a01750b06
brunofoxmulder <bruno@mbsimon.fr>
2026-08-23 00:30:13 +0200

    beta.15: align app version

--- elise_investigator/app/main.py
+++ elise_investigator/app/main.py
@@ -15,7 +15,7 @@ from models import InvestigationRequest
 from proof_policy import StrictInvestigator
 from ui import INDEX_HTML
 
-VERSION = "0.1.0-beta.14"
+VERSION = "0.1.0-beta.15"
 DATA_DIR = Path("/data")
 TOKEN_FILE = DATA_DIR / "api_token"
 OPTIONS_FILE = DATA_DIR / "options.json"
